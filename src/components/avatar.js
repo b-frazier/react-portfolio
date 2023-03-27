4864f8598cfd860fd151bcfdc39adf8180f10721
bfrazier <bthehippie2@gmail.com>
2023-03-27 18:55:36 -0400

    file for image

---
+++ src/components/avatar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Avatar() {
+  return (
+    <img
+      src="../assets/bianca-headshot-square-1mb.jpg"
+      alt=""
+      className="img-thumbnail"
+    ></img>
+  );
+}
+
+export default Avatar;
