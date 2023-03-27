5ab20fcce8f5e3c9f81cbe16507e36d1721c839a
bfrazier <bthehippie2@gmail.com>
2023-03-26 22:09:24 -0400

    creat nav bar

---
+++ src/components/header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function header() {
+  return (
+    <nav>
+      <div>
+        <h2>Bianca Frazier</h2>
+      </div>
+      <div>
+        <h3>About Me</h3>
+      </div>
+      <div>
+        <h3>Portfolio</h3>
+      </div>
+      <div>
+        <h3>Resume</h3>
+      </div>
+      <div>
+        <h3>Contact Me</h3>
+      </div>
+    </nav>
+  );
+}
+
+export default header;
