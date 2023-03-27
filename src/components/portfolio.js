539ddab64382352e8da6f508aabdb70abbf68915
bfrazier <bthehippie2@gmail.com>
2023-03-27 18:54:17 -0400

    create file to serve up to App.js

---
+++ src/components/portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import About from './about';
+import Header from './header';
+
+export default function Portfolio() {
+  return (
+    <div>
+      <Header />
+      <About />
+    </div>
+  );
+}
