78dcadbd17839f84656d6f5a649199e763e1274b
bfrazier <bthehippie2@gmail.com>
2023-03-27 18:56:16 -0400

    create about info

---
+++ src/components/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="container">
+      <h2>I'm Bianca!</h2>
+      <p>
+        I am a Junior Web Developer based in Columbus, Ohio. I have passion for
+        creativity, technology and a good cup of coffee.
+      </p>
+    </div>
+  );
+}
+
+export default About;
