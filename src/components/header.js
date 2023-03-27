5ea15f8da82e1b655ab01ed21da8ef557ce08c21
bfrazier <bthehippie2@gmail.com>
2023-03-27 18:54:54 -0400

    add header content; bootstrap styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,27 @@
 import React from 'react';
 
-function header() {
+function Header() {
   return (
-    <nav>
-      <div>
-        <h2>Bianca Frazier</h2>
-      </div>
-      <div>
-        <h3>About Me</h3>
-      </div>
-      <div>
-        <h3>Portfolio</h3>
-      </div>
-      <div>
-        <h3>Resume</h3>
-      </div>
-      <div>
-        <h3>Contact Me</h3>
+    <nav className="navbar">
+      <div className="container">
+        <div>
+          <h2>Bianca Frazier</h2>
+        </div>
+        <div>
+          <h3>About Me</h3>
+        </div>
+        <div>
+          <h3>Portfolio</h3>
+        </div>
+        <div>
+          <h3>Resume</h3>
+        </div>
+        <div>
+          <h3>Contact Me</h3>
+        </div>
       </div>
     </nav>
   );
 }
 
-export default header;
+export default Header;
