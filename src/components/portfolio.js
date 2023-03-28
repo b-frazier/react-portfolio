9e3915c06609c54a578f298fa5719f764e25c26a
bfrazier <bthehippie2@gmail.com>
2023-03-27 21:39:07 -0400

    add resume info

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import About from './about';
 import Header from './header';
+import Resume from './resume';
 
 export default function Portfolio() {
   return (
     <div>
       <Header />
       <About />
+      <Resume />
     </div>
   );
 }
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,5 +1,139 @@
 import React from 'react';
 
-function Resume() {}
+function Resume() {
+  return (
+    <div className="container">
+      <div>
+        <h3>Education</h3>
+        <h4>The Ohio State University | Columbus, Ohio</h4>
+        <h5>
+          <i>Full Stack Web Development Bootcamp | October 2022 - Present</i>
+        </h5>
+        <h4>Iowa State University | Ames, Iowa</h4>
+        <h5>
+          <i>Bachelor of Industrial Design | August 2017 - December 2021</i>
+        </h5>
+      </div>
+      <div>
+        <h3>Experience</h3>
+        <h4>Analytics Project Manager | Simantel</h4>
+        <h5>Remote | October 2022 - Present</h5>
+        <ul>
+          <li>
+            Lead stand-up meetings and ensure continued progress toward meeting
+            delivery dates
+          </li>
+          <li>Own the analytics quality control process</li>
+          <li>
+            Ensure quality control is carried out as needed on all reporting and
+            analytics projects
+          </li>
+          <li>
+            Work across agency teams to ensure tracking is setup, insights are
+            developed, and optimizations are recommended to the client team
+          </li>
+        </ul>
+        <h4>Associate Program Manager | Simantel</h4>
+        <h5> Remote | August 2022 - September 2022</h5>
+        <ul>
+          <li>
+            Worked closely with the program manager and teams across the agency
+            to produce status reports (both internal and external) on a regular
+            cadence. This included creating and updating dashboards for program
+            enrollment metrics, launch metrics, etc.
+          </li>
+          <li>
+            Served as a key reviewer of all content that feeds the program(s),
+            ensuring that tagging, naming, and brand guidelines are followed
+          </li>
+          <li>
+            Owned managed the overall timelines, reporting schedules and
+            internal standup meetings, and kept all key internal teams informed
+          </li>
+          <li>
+            Assisted in scoping and resource management of new/additional
+            workstreams and projects related to the program
+          </li>
+          <li>Provided support to the overall program content strategy</li>
+        </ul>
+        <h4>College Women's Ministry Director | Blueprint Church</h4>
+        <h5>Atlanta, Georgia | January 2022 - July 2022</h5>
+        <ul>
+          <li>Mentored and counseled college aged women</li>
+          <li>
+            Designed branded ministry merchandise, social media content and
+            promotional materials
+          </li>
+          <li>Formulated and implement campus outreach strategies</li>
+          <li>
+            Created and schedule special events for the general college ministry
+          </li>
+          <li>Served alongside 4 other staff members</li>
+        </ul>
+        <h4>Design Team Intern | CIKK Apparel</h4>
+        <h5>Ames, Iowa | December 2018 - October 2020</h5>
+        <ul>
+          <li>
+            Sourced new products that fit within the brand image and appealed to
+            the target consumer
+          </li>
+          <li>
+            Researched current color and style trends to create relevant,
+            timeless products
+          </li>
+          <li>
+            Designed shipping and marketing materials that better fit within the
+            brand image
+          </li>
+          <li>
+            Provided creative design feedback to photographers and graphic
+            designers
+          </li>
+          <li>
+            Worked closely with marketing and fulfillment teams to develop a
+            satisfying customer experience
+          </li>
+        </ul>
+        <h4>Community Adviser | Iowa State University</h4>
+        <h5>Ames, Iowa | August 2018 - May 2019</h5>
+        <ul>
+          <li>
+            Fostered and maintained an inclusive community among 52 college
+            women
+          </li>
+          <li>Enforced and upheld university policies</li>
+          <li>
+            Served as an intermediary between residents and Iowa State
+            Department of Residence
+          </li>
+          <li>
+            Advised and oversaw the five member house cabinet as well as the
+            yearly budget of $2,000
+          </li>
+          <li>
+            Actively encouraged relationships between persons of different
+            social and cultural backgrounds
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3>Skills, Technical Tools & Related Coursework</h3>
+        <div>
+          <div>
+            <ul>
+              <li>Git, GitHub, Visual Studio Code</li>
+              <li>HTML, CSS, JavaScript</li>
+              <li>Figma, Adobe Suite, Solidworks</li>
+              <li>Asana, Notion, Jira, Slack</li>
+              <li>Human Factors & Ergonomics, Computer Aided Design</li>
+              <li>Design Thinking, Design Engineering</li>
+              <li>Design Research Methods, Digital Design Technologies</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Resume;
