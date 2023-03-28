b29f4e7605240cab12d0f8933eb862be9198a63d
bfrazier <bthehippie2@gmail.com>
2023-03-27 22:21:40 -0400

    render pages conditionally

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import About from './about';
 import Header from './header';
 import Resume from './resume';
 
 export default function Portfolio() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if (currentPage === 'Contact') {
+      return '';
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    if (currentPage === 'Projects') {
+      return '';
+    }
+    return <About />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <div>
-      <Header />
-      <About />
-      <Resume />
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
     </div>
   );
 }
