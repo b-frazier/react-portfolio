6d0fede3b38fc8d2cb5370ba569af39f37a8cb82
bfrazier <bthehippie2@gmail.com>
2023-03-27 23:38:12 -0400

    add footer with socials

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React from 'react';
+import github from '../assets/github.png';
+import linkedin from '../assets/linkedin.png';
 
-function Footer(){
-    return()
+function Footer() {
+  return (
+    <nav className="navbar fixed-bottom bg-light">
+      <div className="container" style={{ justifyContent: 'center' }}>
+        <a
+          className="nav-link"
+          href="https://github.com/b-frazier"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            src={github}
+            style={{ width: 30, marginRight: 5 }}
+            alt="github logo"
+          ></img>
+        </a>
+        <a
+          className="nav-link"
+          href="https://www.linkedin.com/in/bianca-frazier-ba6170172/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            src={linkedin}
+            style={{ width: 30, marginLeft: 5 }}
+            alt="github logo"
+          ></img>
+        </a>
+      </div>
+      <div
+        className="container"
+        style={{ justifyContent: 'center', marginTop: 3 }}
+      >
+        <p style={{ margin: 0 }}>Find me on my socials.</p>
+      </div>
+    </nav>
+  );
 }
 
 export default Footer;
