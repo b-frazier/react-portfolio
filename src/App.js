4fd8c925966aec25bb217f6abea60b860fb4a8c1
bfrazier <bthehippie2@gmail.com>
2023-03-27 22:23:35 -0400

    remove default react code

--- src/App.js
+++ src/App.js
@@ -4,24 +4,6 @@ import Portfolio from './components/portfolio';
 
 function App() {
   return <Portfolio />;
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
