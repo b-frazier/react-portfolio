71eff9966ce4c4ca2c547a0f3af1d0a299797d1d
bfrazier <bthehippie2@gmail.com>
2023-03-27 22:55:40 -0400

    insert form with bootstrap; update for react syntax

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,55 @@
 import React from 'react';
 
-function Contact() {}
+function Contact() {
+  return (
+    <div className="container my-5">
+      <div className="row">
+        <div className="col">
+          <label for="firstName" className="form-label">
+            First Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="First name"
+            aria-label="First name"
+          />
+        </div>
+        <div className="col">
+          <label for="lastName" className="form-label">
+            Last Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Last name"
+            aria-label="Last name"
+          />
+        </div>
+      </div>
+      <div className="mb-3">
+        <label for="exampleFormControlInput1" className="form-label">
+          Email address
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          id="exampleFormControlInput1"
+          placeholder="name@example.com"
+        />
+      </div>
+      <div className="mb-3">
+        <label for="exampleFormControlTextarea1" className="form-label">
+          Send a message!
+        </label>
+        <textarea
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+        ></textarea>
+      </div>
+    </div>
+  );
+}
 
 export default Contact;
