689f35c22a25b55de2818b01acb909ac3d4dfee1
bfrazier <bthehippie2@gmail.com>
2023-03-27 22:49:26 -0400

    import Avatar; add more to bio

--- src/components/about.js
+++ src/components/about.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import Avatar from './avatar';
 
 function About() {
   return (
     <div className="container my-5">
-      <h2>I'm Bianca!</h2>
-      <p>
-        I am a Junior Web Developer based in Columbus, Ohio. I have passion for
-        creativity, technology and a good cup of coffee.
-      </p>
+      <Avatar />
+      <div className="mx-auto my-3" style={{ width: 400 }}>
+        <h2>I'm Bianca!</h2>
+        <p>
+          I am a Junior Web Developer based in Columbus, Ohio. I have passion
+          for creativity, technology and a good cup of coffee.
+        </p>
+        <p>
+          I grew up and attended university in Iowa but after graduating, I
+          packed up and headed for the big city! I'm a few months shy of an
+          amazing year of being a Buckeye. O-H-I-O!
+        </p>
+      </div>
     </div>
   );
 }
